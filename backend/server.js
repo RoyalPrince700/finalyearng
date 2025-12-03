b635bee50885edd3cf6b7f415aa699ecb0c8b3cc
Royal Joseph <royalprincecube@gmail.com>
2025-12-03 21:18:58 +0100

    update the server package and vercel to allow the vercel communicate with backend

--- backend/server.js
+++ backend/server.js
@@ -52,6 +52,10 @@ app.use('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 FinalYearNG Server running on port ${PORT}`);
-});
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`🚀 FinalYearNG Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
